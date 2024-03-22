b0d7ff38335237714ea46632075113a9cfe6cd66
btrizzzy <147527808+btrizzzy@users.noreply.github.com>
2024-03-21 22:51:17 -0600

    feat: add assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,25 @@
 /* Accordion Buttons */
-var acc = document.getElementsByClassName("accordion");
-var i;
+const acc = document.querySelectorAll(".accordion");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+acc.forEach(button => {
+  button.addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+    const panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
   });
-}
+});
 
 /* Changing Copyright Year */
-const currentYear = document.getElementById('currentyear')
-let date = new Date();
-let year = date.getFullYear();
+const currentYear = document.getElementById('currentyear');
+const date = new Date();
+const year = date.getFullYear();
 
-currentYear.innerHTML = '&copy; ' + year + ' Briana Truong. All rights reserved.';
+currentYear.innerHTML = `&copy; ${year} Briana Truong. All rights reserved.`;
